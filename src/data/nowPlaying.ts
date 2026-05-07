e1ffa6f7a0f81ef710b2688925a02bbe0cff160a
James Schoepf <nadaklu@gmail.com>
2026-05-07 14:06:07 -0400

    Add High Octane HQ link for Trans Am build

--- src/data/nowPlaying.ts
+++ src/data/nowPlaying.ts
@@ -1,5 +1,5 @@
 export const nowPlaying = [
   { label: 'PLAYING', value: 'Diablo IV: Lord of Hatred', icon: 'gamepad' },
-  { label: 'WRENCHING', value: '1986 Trans Am', icon: 'wrench' },
+  { label: 'WRENCHING', value: '1986 Trans Am', icon: 'wrench', href: 'https://highoctanehq.com/garage/98058824-3a2d-4d4c-8eae-279072bb9860' },
   { label: 'READING', value: 'The Let Them Theory', icon: 'book', href: 'https://amzn.to/4cURfTI' },
 ];
