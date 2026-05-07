46e77ac3f61c16fa6420722c446aa0576a8c03db
James Schoepf <nadaklu@gmail.com>
2026-05-07 13:35:23 -0400

    Correct Diablo IV expansion title in Now Playing

--- src/data/nowPlaying.ts
+++ src/data/nowPlaying.ts
@@ -1,5 +1,5 @@
 export const nowPlaying = [
-  { label: 'PLAYING', value: 'Diablo IV: Vessel of Hatred', icon: 'gamepad' },
+  { label: 'PLAYING', value: 'Diablo IV: Lord of Hatred', icon: 'gamepad' },
   { label: 'WRENCHING', value: '1986 Trans Am', icon: 'wrench' },
   { label: 'READING', value: 'The Let Them Theory', icon: 'book', href: 'https://amzn.to/4cURfTI' },
 ];
